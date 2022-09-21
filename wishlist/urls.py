2cb38611c942bb21102847f31af7358d51b2de86
arya.daffa1 <arya.daffa@gmail.com>
2022-09-21 23:43:16 +0700

    update lab-03 pbp

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -4,6 +4,9 @@ from wishlist.views import xml_funct
 from wishlist.views import json_funct
 from wishlist.views import id_funct_json
 from wishlist.views import id_funct_xml
+from wishlist.views import register
+from wishlist.views import login_user
+from wishlist.views import logout_user
 
 app_name = 'wishlist'
 
@@ -13,4 +16,7 @@ urlpatterns = [
     path('json/', json_funct, name='json_funct'),
     path('xml/<int:id>', id_funct_xml, name='id_funct_xml'),
     path('json/<int:id>', id_funct_json, name='id_funct_json'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
